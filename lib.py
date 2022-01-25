f66c97ca8e9dd47f4040653e11e06ff3b68bc73a
B100 <69177552+B100GH@users.noreply.github.com>
2022-01-25 19:18:30 +0000

    Added the following pages - Blog (Under Development) - resume (Under Development)

--- lib.py
+++ lib.py
@@ -1,5 +1,3 @@
-
-
 def getkey():
   r = open('gpgkey.html','r')
   content = r.read()
@@ -8,6 +6,18 @@ def getkey():
 
 def gethome():
   r = open('home.html','r')
+  content = r.read()
+  r.close()
+  return content
+
+def getblog():
+  r = open('blog.html','r')
+  content = r.read()
+  r.close()
+  return content
+
+def getresume():
+  r = open('resume.html','r')
   content = r.read()
   r.close()
   return content
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import flask
+from flask import Flask
 from lib import getkey
 from lib import gethome
+from lib import getblog
+from lib import getresume
 
-from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,6 +15,17 @@ def home():
 def gpg():
     gpgkey = getkey()
     return gpgkey
+
+@app.route("/blog")
+def blog():
+    blog = getblog()
+    return blog
+
+@app.route("/resume")
+def resume():
+    resume = getresume()
+    return resume
+
 
 if __name__ == "__main__":
   app.run(debug=True,host='0.0.0.0',port='8080')
